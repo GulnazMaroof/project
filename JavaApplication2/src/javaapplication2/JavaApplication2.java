be9183b1af0f7642848f69f630aa8d59a426a812
M i Computers mcd <M i Computers mcd@DESKTOP-LR56AM6>
2017-03-14 14:02:13 -0700

    gulnaz maroof

---
+++ JavaApplication2/src/javaapplication2/JavaApplication2.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication2;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author M i Computers mcd
+ */
+import java.util.Scanner;
+public class JavaApplication2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+         Scanner sc=new Scanner(System.in);
+    int d;
+    
+    System.out.println("enter the value of a");
+    d=sc.nextInt();
+    System.out.println(d);
+    
+        // TODO code application logic here
+    }
+    
+}
