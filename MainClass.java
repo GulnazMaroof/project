b7c2c8d87ee27bef888e8b0a7f1bdadfce468fd7
GulnazMaroof <gullmaroof2@gmail.com>
2017-03-20 21:13:04 -0800

    it's my main class

---
+++ MainClass.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Prery Doll
+ */
+public class MainClass {
+    public static void main(String[] args)
+    {
+        Chair c1=new Chair();
+        c1.setlegs(2);
+       c1.setcolor("brown");
+        System.out.println("c1.getlegs");
+        System.out.println("c1.getcolor");
+    }
+}
+                
+                
+                
+                
+                
+                
+                
+                
+                
+          
+                        
+    
+    
+
