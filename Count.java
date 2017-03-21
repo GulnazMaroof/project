6b87018e717e8b219671c886295dbfaf58c2152c
GulnazMaroof <gullmaroof2@gmail.com>
2017-03-20 20:56:54 -0800

    my class

---
+++ Count.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Prery Doll
+ */
+public class Count {
+    
+public static void main(String[] args)
+{
+//----------------------------------------------------------------   //  Concatenates and adds two numbers and prints the results.   //----------------------------------------------------------------   public static void main (String[] args)   {      System.out.println ("24 and 45 concatenated: " + 24 + 45);
+      System.out.println ("24 and 45 added: " + (24 + 45));
+}
+}
