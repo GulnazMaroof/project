6c6d8c3468a46c790e68d339cb1969c5c7ebbf93
GulnazMaroof <gullmaroof2@gmail.com>
2017-03-20 20:52:55 -0800

    it's my new class

---
+++ Chair.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Prery Doll
+ */
+public class Chair {
+    private int legs;
+    private String color;
+public Chair()
+{
+legs=0;
+color=" ";
+}
+public void setlegs(int l)
+{
+legs=l;
+}
+public int getlegs()
+{
+return legs;
+}
+public void setcolor(String c)
+{
+color=c;
+}
+public String getcolor()
+{
+return color;
+}
+    
+}
